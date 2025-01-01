108fa01d77579482d1e071e90dd09f60795c2c49
Shamkhal <shamkhalb@outlook.com>
2025-01-01 20:16:28 +0400

    make publishable config

--- src/Providers/TelegramMailServiceProvider.php
+++ src/Providers/TelegramMailServiceProvider.php
@@ -13,21 +13,21 @@ class TelegramMailServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Publish config so the user can customize it
-        // $this->publishes([
-        //     __DIR__ . '/../config/mail-telegram.php' => config_path('mail-telegram.php'),
-        // ], 'telegram-mail-config');
+        // Publish the config file
+        $this->publishes([
+            __DIR__ . '/../config/mail-telegram.php' => config_path('mail-telegram.php'),
+        ], 'telegram-mail-config');
 
         // Extend the mail manager to add a 'telegram' driver.
         // Extend the mail manager
         $this->app->resolving('mail.manager', function (MailManager $mailManager) {
             // Create a "telegram" driver
-            $mailManager->extend('telegram', function (array $config) {
+            $mailManager->extend('telegram', function () {
                 // Instead of $config['bot_token'] etc., we'll use $config['username'] and $config['password']
                 // because we are intentionally using MAIL_USERNAME for the chat_id, and MAIL_PASSWORD for bot_token.
 
                 $chatId = config('mail-telegram.chat_id');
-                $botToken = config('mail-telegram.bot_token'); 
+                $botToken = config('mail-telegram.bot_token');
 
                 return new TelegramTransport($chatId, $botToken);
             });
